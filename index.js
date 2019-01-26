3990b145f8e409f846b9cae6503b549e97c9dc6c
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-01-26 12:50:22 -0800

    corrected js and link between js and html

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-$(function() {
-    $('#js-shopping-list-form').submit( event => {
-        event.preventDefault()
+$(function addItem() {
+    $('#js-shopping-list-form').submit(event => {
+        event.preventDefault();
 
-        let userInput = $('#shopping-list-entry').val()
+        let userInput = $(this).find('#shopping-list-entry').val();
 
-        let item = $('.shopping-list').append(
+        let item = $('ul').append( 
             $( `
                 <li>
-                    <span class= "shopping-item">${userInput}</span>
-                    <div class= "shopping-item-controls">
-                        <button class= "shopping-item-toggle">
-                            <span class= "button-label">check</span>
+                    <span class="shopping-item">${userInput}</span>
+                    <div class="shopping-item-controls">
+                        <button class="shopping-item-toggle">
+                            <span class="button-label">check</span>
                         </button>
-                    </div>
+                        <button class="shopping-item-delete">
+                            <span class="button-label">delete</span>
+                        </button>
+                </div>
                 </li>
-            `))
-        
-    })
+            ` ))
+
+    });
 })
 
 function deleteItem() {
-    $('ul').on('click', '.shopping-item-delete', funcion(event) {
-        $(this).parents('li').remove()
-    })
+    $('ul').on('click', '.shopping-item-delete', function(event) {
+        $(this).parents('li').remove();
+      });
 }
 
-$(deleteItem)
+$(deleteItem);
 
 function checkItem() {
     $('ul').on('click', '.shopping-item-toggle', function(event) {
-        $(this).closest('li').find($('.shopping-item')).toggleClass('shopping-item_shecked')
-    })
+        $(this).closest('li').find($('.shopping-item')).toggleClass('shopping-item__checked');
+      });
 }
 
-$(checkItem)
+$(checkItem);
