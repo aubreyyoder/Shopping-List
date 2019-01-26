4ea1d4bdc1fc14fdac09643acdf326165f9875bc
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-01-26 14:16:06 -0800

    Simplified js

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-$(function addItem() {
-    $('#js-shopping-list-form').submit(event => {
-        event.preventDefault()
+(function($, window, document) {
+    // The $ is now locally scoped
 
-        let userInput = $(this).find('#shopping-list-entry').val()
+    // Check button functionality
+    $(document).on('click', '.shopping-item-toggle', function(){
+        $(this).parent().prev().toggleClass('shopping-item__checked')
+    });
 
-        let item = $('ul').append( 
-            $( `
-                <li>
-                    <span class="shopping-item">${userInput}</span>
-                    <div class="shopping-item-controls">
-                        <button class="shopping-item-toggle">
-                            <span class="button-label">check</span>
-                        </button>
-                        <button class="shopping-item-delete">
-                            <span class="button-label">delete</span>
-                        </button>
-                </div>
-                </li>
-            ` ))
+    // Delete button functionality. Deletes parent <li>
+    $(document).on('click', '.shopping-item-delete', function() {
+        $(this).parents('li').remove()
+    });
+
+    const $form = $('#js-shopping-list-form');
+    const $textInput = $form.find('#shopping-list-entry');
+    const $shoppingList = $('.shopping-list');
+
+    // Adds item to shopping list
+    $form.submit(function(event) {
+        event.preventDefault();
+        const value = $textInput.val();
+        $shoppingList.append(`
+        <li>
+        <span class="shopping-item">${value}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>
+      `);
+
+      // Resets value of input to placeholder
+        $textInput.val('');
 
     })
-})
 
-function deleteItem() {
-    $('ul').on('click', '.shopping-item-delete', function(event) {
-        $(this).parents('li').remove();
-      })
-}
-
-$(deleteItem)
-
-function checkItem() {
-    $('ul').on('click', '.shopping-item-toggle', function(event) {
-        $(this).closest('li').find($('.shopping-item')).toggleClass('shopping-item__checked');
-      })
-}
-
-$(checkItem)
+}(window.jQuery, window, document));
